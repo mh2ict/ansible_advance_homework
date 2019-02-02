9afbde2e7b244d7ac0b074e8a07a9796c81936cf
root <root@bastion.bf93.example.opentlc.com>
2019-02-02 13:53:50 +0000

    version 1.1.2

---
+++ env_opentlc.sh
@@ -0,0 +1,8 @@
+export TOWER_GUID=bf93
+export OSP_GUID=7171
+export OPENTLC_LOGIN=mhaque-redhat.com
+export OPENTLC_PASSWORD=Redhat!23
+export GITHUB_REPO=https://github.com/mh2ict/ansible_advance_homework
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=RegionOne
+export RH_MAIL_ID=mhaque@redhat.com
