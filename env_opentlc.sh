f42137b10877980a1c7775de472f095044d212ef
root <root@bastion.43d0.example.opentlc.com>
2019-02-03 14:21:10 +0000

    Update files

--- env_opentlc.sh
+++ env_opentlc.sh
@@ -1,5 +1,5 @@
-export TOWER_GUID=bf93
-export OSP_GUID=7171
+export TOWER_GUID=43d0
+export OSP_GUID=445f
 export OPENTLC_LOGIN=mhaque-redhat.com
 export OPENTLC_PASSWORD=Redhat!23
 export GITHUB_REPO=https://github.com/mh2ict/ansible_advance_homework
